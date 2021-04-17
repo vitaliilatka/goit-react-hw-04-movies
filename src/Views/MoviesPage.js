7e4c745a2f684da322e480ce701aeb1a6196fe6e
vetallatka <latka.vitalik@yandex.ru>
2021-04-17 12:11:49 +0200

    made MoviesPage

--- src/Views/MoviesPage.js
+++ src/Views/MoviesPage.js
@@ -1,5 +1,51 @@
+import { useState, useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import filmsApi from '../Services/Films.Api';
+import MoviesList from '../Components/MoviesList';
+
 const MoviesPage = () => {
-    return <h1>Это страница поиска фильмов</h1>;
+    const history = useHistory();
+    const location = useLocation();
+    const [inputquery, setInputQuery] = useState(location?.state?.query || '');
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        if (!inputquery) return;
+        filmsApi.searchMovie(inputquery).then(query => {
+            setMovies(query);
+        });
+    }, []);
+
+    const handleChange = e => {
+        setInputQuery(e.currentTarget.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        filmsApi.searchMovie(inputquery).then(query => {
+            setMovies(query);
+            history.push({ ...location, search: `?query=${inputquery}` });
+        });
+    };
+    return (
+        <>
+            <h1>Search your favourite movie</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    value={inputquery}
+                    autoComplete='off'
+                    autoFocus
+                    onChange={handleChange}
+                />
+                <button type='submit'>
+                    Search
+                </button>
+            </form>
+            {movies.length === 0 && <p>No results</p>}
+            <MoviesList movies={movies} query={inputquery} />
+        </>
+    );
 };
 
 export default MoviesPage;
